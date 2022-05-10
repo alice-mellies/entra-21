08dbdb5e69e1c4bbadf1ee6114835c9118d5a2c7
Alice Mellies <melliesalice@gmail.com>
2022-05-09 21:59:37 -0300

    Exercicio 5, 6 e 7 finalizados.

--- Entra21.ExerciciosWhile/Exercicio05.cs
+++ Entra21.ExerciciosWhile/Exercicio05.cs
@@ -10,10 +10,51 @@ namespace Entra21.ExerciciosWhile
     {
         public void Executar()
         {
-            int quantidade = 0;
-            int ano = 0;
-            string modelo = "";
-            double valor = 0;
+            int quantidadeCarros = 0;
+            int contador = 0;
+            int mediaAno = 0;
+            int somaAno = 0;
+            double somaValor = 0;
+            double mediaValor = 0;
+            int quantidadeA = 0;
+            int quantidadeG = 0;
+            Console.WriteLine("Digite a quantidade de carros a cadastrar");
+            quantidadeCarros = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            while(contador < quantidadeCarros)
+            {
+                Console.WriteLine("Digite o modelo do carro: ");
+                string modeloCarro = Console.ReadLine().ToLower().Trim();
+                Console.WriteLine("Digite o valor do carro");
+                double valorCarro = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Digite o ano do carro: ");
+                int anoCarro = Convert.ToInt32(Console.ReadLine());
+                contador = contador + 1;
+                somaAno = somaAno + anoCarro;
+                somaValor = somaValor + valorCarro;
+                if(modeloCarro.StartsWith("a") == true)
+                {
+                    quantidadeA = quantidadeA + 1;
+                }
+                if (modeloCarro.StartsWith("g") == true)
+                {
+                    quantidadeG = quantidadeG + 1;
+                }
+
+
+            }
+            mediaAno = somaAno / quantidadeCarros;
+            mediaValor = mediaValor / quantidadeCarros;
+
+            Console.WriteLine("A média do ano dos carros é: " + mediaAno);
+            Console.WriteLine("A média do valor dos carro é: " + somaAno);
+            Console.WriteLine("A quantidade de carros que começa com a letra A é: " + quantidadeA);
+            Console.WriteLine("A quantidade de carros que começa com a letra G é: " + quantidadeG);
+
+            
+          
 
 
         }
---
+++ Entra21.ExerciciosWhile/Exercicio07.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosWhile
+{
+    internal class Exercicio07
+    {
+       public void Executar()
+        {
+            int quantidadeProdutos = 0;
+            double valorProduto = 0;
+            double valorPedido = 0;
+
+            while(quantidadeProdutos < 5)
+
+            {
+                Console.WriteLine("Digite o nome do produto");
+                string nomeProduto = Console.ReadLine();
+                Console.WriteLine("Dgite o valor do produto: ");
+                valorProduto = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Digite a quantidade de produtos: ");
+                quantidadeProdutos = Convert.ToInt32(Console.ReadLine());
+                valorPedido = valorProduto + quantidadeProdutos;
+                valorPedido = valorPedido + valorProduto;
+
+                
+
+            }
+            Console.WriteLine("O valor do pedido sem desconto é: " + valorPedido );
+            double valorPedidoDesconto = valorPedido - 150;
+            Console.WriteLine("O valor do pedido com desconto é: " + valorPedidoDesconto);
+
+        }
+    }
+}
