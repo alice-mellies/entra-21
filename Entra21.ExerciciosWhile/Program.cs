046e27496fbc5469dfc9fca7274e1c5a27fc75c9
Alice Mellies <melliesalice@gmail.com>
2022-05-09 22:11:27 -0300

    progams

--- Entra21.ExerciciosWhile/Program.cs
+++ Entra21.ExerciciosWhile/Program.cs
@@ -9,7 +9,17 @@ Console.WriteLine(@"---------MENU--------
 4 - Exercicio 04
 5 - Exercicio 05
 6 - Exercicio 06
-7 - Exercicio 07");
+7 - Exercicio 07
+8 - Exercicio 08
+9 - Exercicio 09
+10 - Exercicio 10
+11 - Exercicio 11
+12 - Exercicio 12
+13 - Exercicio 13
+14 - Exercicio 14
+15 - Exercicio 15
+16 - Exercicio 16
+17 - Exercicio 17");
 
 Console.WriteLine("Digite a opção desejada: ");
 int opcaoDesejada = Convert.ToInt32(Console.ReadLine());
@@ -44,6 +54,17 @@ if (opcaoDesejada == 6)
     Exercicio06 exercicio6 = new Exercicio06();
     exercicio6.Executar();
 }
+if (opcaoDesejada == 7)
+{
+    Exercicio07 exercicio7 = new Exercicio07();
+    exercicio7.Executar();
+}
+if (opcaoDesejada == 8)
+{
+    Exercicio06 exercicio6 = new Exercicio06();
+    exercicio6.Executar();
+}
+
 
 
 
